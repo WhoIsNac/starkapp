c5ad4820149dc0c2a89a0221326ade3bc811c898
Nacer Fl <90337466+NacerFl@users.noreply.github.com>
2023-07-28 02:36:02 +0200

    argent x and braavos integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from "react-redux";
+import {store} from "./store/store";
+import {
+  Container,
+  CssBaseline,
+} from '@mui/material';
+
+import { Header } from './wallet/wallet';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <Container maxWidth="sm">
+      <Header />
+    </Container>
+  </ThemeProvider>
+  </Provider>
   );
 }
 
---
+++ src/store/store.ts
@@ -0,0 +1,23 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import walletReducer from '../wallet/walletEngine';
+
+export const store = configureStore({
+  reducer: {
+    wallet: walletReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+      immutableCheck: false,
+    }),
+});
+
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
